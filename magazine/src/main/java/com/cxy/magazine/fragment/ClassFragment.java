bd59d2541982ec5b42b8045f75bfb0e3c3cc1e3d
caoxingyu <caoxingyu2016@qq.com>
2018-01-12 16:06:33 +0800

    新增杂志分类

--- magazine/src/main/java/com/cxy/magazine/fragment/ClassFragment.java
+++ magazine/src/main/java/com/cxy/magazine/fragment/ClassFragment.java
@@ -3,8 +3,12 @@ package com.cxy.magazine.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +16,23 @@ import android.widget.TextView;
 
 import com.cxy.magazine.R;
 import com.cxy.magazine.util.ACache;
+import com.cxy.magazine.util.Util;
 import com.github.jdsjlzx.recyclerview.LRecyclerView;
+import com.github.jdsjlzx.recyclerview.LRecyclerViewAdapter;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -28,7 +42,8 @@ public class ClassFragment extends Fragment {
     public static final String MAGAZIENE_URL="http://www.fx361.com";
     private List<HashMap> magazineList;
     private static final int LOAD_FINISHED=100;
-    private MagazineAdapter adapter;
+    private MagazineAdapter adapter=null;
+    private LRecyclerViewAdapter mLRecyclerViewAdapter = null;
     private ACache mAcache;
 
 
@@ -48,11 +63,86 @@ public class ClassFragment extends Fragment {
 
         View view=inflater.inflate(R.layout.fragment_class, container, false);
         unbinder = ButterKnife.bind(this, view);
+        magazineList =new ArrayList<HashMap>();
+        mAcache=ACache.get(getContext());
+        setLRecyclerview();
+        Thread thread=new getHtml();
+        thread.start();
         return  view;
     }
     public void setLRecyclerview(){
+        GridLayoutManager manager=new GridLayoutManager(this.getContext(),3);
+        mLRecyclerview.setLayoutManager(manager);
+        adapter=new MagazineAdapter();
+        mLRecyclerViewAdapter=new LRecyclerViewAdapter(adapter);
+        mLRecyclerview.setAdapter(mLRecyclerViewAdapter);
 
     }
+
+    @OnClick(R.id.tv_search)
+    public void searchClick(){
+        //  Util.toastMessage(getActivity(),"searchView");
+        //跳转Fragment
+      /*  getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, SearchFragment.newInstance())
+                .addToBackStack(null)
+                .commit();*/
+
+
+    }
+
+
+    class  getHtml extends Thread{
+        @Override
+        public void run() {
+            try {
+                Document docHtml=null;
+                String magazineClass=mAcache.getAsString("magazineClass");
+                if (!TextUtils.isEmpty(magazineClass)){
+                    docHtml= Jsoup.parse(magazineClass);
+                }else{
+                    docHtml = Jsoup.connect(MAGAZIENE_URL).get();
+                    mAcache.put("magazineClass",docHtml.toString(),30* ACache.TIME_DAY);
+                }
+
+                Elements kinds=docHtml.getElementsByClass("navBox");
+                for (Element kind : kinds){
+
+                    Elements alist=kind.getElementsByTag("a");
+                    for (Element a : alist){
+                        HashMap kindMap=new HashMap<String,String>();
+                        kindMap.put("text",a.text());
+                        kindMap.put("href",a.attr("href"));
+
+                        magazineList.add(kindMap);
+                    }
+
+                }
+
+                handler.sendEmptyMessage(LOAD_FINISHED);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Util.toastMessage(getActivity(),e.toString());
+            }
+
+        }
+    }
+
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what){
+                case LOAD_FINISHED:
+                    adapter.notifyDataSetChanged();
+
+                    break;
+
+            }
+        }
+    };
+
 
     @Override
     public void onDestroyView() {
@@ -65,7 +155,7 @@ public class ClassFragment extends Fragment {
 
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(getContext()).inflate(R.layout.magazine_item, parent, false));
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(getContext()).inflate(R.layout.magazine_class_item, parent, false));
             return holder;
         }
 
@@ -78,10 +168,10 @@ public class ClassFragment extends Fragment {
             holder.tvClassName.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent=new Intent(getContext(), MagazineActivity.class);
+                    /*Intent intent=new Intent(getContext(), MagazineActivity.class);
                     intent.putExtra("url",MAGAZIENE_URL+"/"+dataMap.get("href").toString());
                     intent.putExtra("title",dataMap.get("text").toString());
-                    startActivity(intent);
+                    startActivity(intent);*/
                 }
             });
         }
