695352013a2845ec96bc4f98b8afdbced8b73f14
caoxingyu <caoxingyu2016@qq.com>
2018-05-23 13:48:35 +0800

    空指针修复

--- magazine/src/main/java/com/cxy/magazine/fragment/MagazineListFragment.java
+++ magazine/src/main/java/com/cxy/magazine/fragment/MagazineListFragment.java
@@ -217,7 +217,7 @@ public class MagazineListFragment extends BaseFragment {
        int currentSize = recycleViewAdapter.getItemCount();
        for (int i = currentSize; i < currentSize+REQUEST_COUNT; i++){
            try {
-               if (dataArray.getJSONObject(i)!=null){
+               if (i<dataArray.length() && dataArray.getJSONObject(i)!=null){
                    dataDisplayArray.put(dataArray.getJSONObject(i));
                    mCurrentCounter += 1;
                }
@@ -231,7 +231,7 @@ public class MagazineListFragment extends BaseFragment {
 
         for (int i = 0; i < REQUEST_COUNT; i++){
             try {
-                if (dataArray.getJSONObject(i)!=null){
+                if (i<dataArray.length() && dataArray.getJSONObject(i)!=null){
                     dataDisplayArray.put(dataArray.getJSONObject(i));
                     mCurrentCounter += 1;
                 }
